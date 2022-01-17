2624c8a4dce7431b30a2d68cf6527d3ef4ca131e
young.xu <young.xu@convertlab.com>
2022-01-17 18:27:40 +0800

    reduce tuple

--- SparkScalaCourse/src/main/scala/com/sundogsoftware/spark/_2_FriendsByAge.scala
+++ SparkScalaCourse/src/main/scala/com/sundogsoftware/spark/_2_FriendsByAge.scala
@@ -34,7 +34,7 @@ object _2_FriendsByAge {
     
     // Lots going on here...
     // We are starting with an RDD of form (age, numFriends) where age is the KEY and numFriends is the VALUE
-    // We use mapValues to convert each numFriends value to a tuple of (numFriends, 1)
+    // We use mapValues to convert each numFriends value to a tuple of (numFriends, 1), so (22,11) ==> (22, (11,1))
     // Then we use reduceByKey to sum up the total numFriends and total instances for each age, by
     // adding together all the numFriends values and 1's respectively.
     val totalsByAge = rdd.mapValues(x => (x, 1)).reduceByKey( (x,y) => (x._1 + y._1, x._2 + y._2))
